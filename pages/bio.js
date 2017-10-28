3c9b3288158f2f5560971fc5fd5d5c57eddb3770
Anusha <anusha.e@yahoo.com>
2017-10-28 13:52:18 -0400

    adding ng-ref

--- pages/bio.js
+++ pages/bio.js
@@ -1,12 +1,17 @@
 var BioController = function($scope) {
 	
 	$scope.desc = "Hello! Thanks for visiting my webpage.I am Anusha. I am a learner. My passion is";
+	
 	$scope.more = function(){
 		$scope.desc = "Hello! Thanks for visiting my webpage.I am Anusha. I am a learner. My passion is to excel in Front End web development. Currently focusing on learning Angular framework";
 	};
 };
 
 app.controller("BioController",BioController);
-//module.exports = MainController;
 
+
+
+
+
+//module.exports = MainController;
 //to excel in Front End web development. Currently focusing on learning Angular framework.."
