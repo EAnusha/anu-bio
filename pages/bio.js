62777a6af827c28ef68220b7b4e7748effb62728
Anusha <anusha.e@yahoo.com>
2017-10-28 15:47:58 -0400

    added desc

--- pages/bio.js
+++ pages/bio.js
@@ -4,9 +4,10 @@ var BioController = function($scope) {
 	
 	$scope.more = function(){
 		$scope.desc = "Hello! Thanks for visiting my webpage.I am Anusha. I am a learner. My passion is to excel in Front End web development. Currently focusing on learning Angular framework";
-	};
+	
 	$scope.less = function(){
 		$scope.desc = "Hello! Thanks for visiting my webpage.I am Anusha. I am a learner. My passion is";
+	};
 	};
 };
 
