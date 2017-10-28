5f5e5ea6b7e0f8fae7f2151448e277197ad2ee7a
Anusha <anusha.e@yahoo.com>
2017-10-28 16:54:35 -0400

    toggle refactor

--- pages/bio.js
+++ pages/bio.js
@@ -1,7 +1,10 @@
 var BioController = function($scope) {
 	
-	$scope.desc = "Hello! Thanks for visiting my webpage.I am Anusha. I am a learner. My passion is";
-	
+	$scope.displayBio = shortBio;
+	$scope.displayBioButton = "Show More";
+
+	$scope.shortBio = "Hello! Thanks for visiting my webpage.I am Anusha. I am a learner. My passion is";
+	$scope.fullBio = "Hello! Thanks for visiting my webpage.I am Anusha. I am a learner. My passion is to excel in Front End web development. Currently focusing on learning Angular framework";
 	/*$scope.more = function(){
 		$scope.desc = "Hello! Thanks for visiting my webpage.I am Anusha. I am a learner. My passion is to excel in Front End web development. Currently focusing on learning Angular framework";
 	
@@ -10,6 +13,18 @@ var BioController = function($scope) {
 	};
 	};*/
 
+	$scope.bioToggle = function() {
+		if($scope.displayBio==$scope.shortBio) {
+			$scope.displayBio = $scope.fullBio;
+			$scope.displayBioButton = "Show Less";
+		};
+		else if ($scope.displayBio==$scope.fullBio) {
+			$scope.displayBio = $scope.shortBio;
+			$scope.displayBioButton = "Show More";
+		};
+
+	}
+	/*
 	$scope.bioClickMore = function(){
       $scope.desc = "Hello! Thanks for visiting my webpage.I am Anusha. I am a learner. My passion is to excel in Front End web development. Currently focusing on learning Angular framework";
       $scope.less = "less";
@@ -17,7 +32,7 @@ var BioController = function($scope) {
 
 	$scope.bioClickLess = function(){
       $scope.desc = "Hello! Thanks for visiting my webpage.I am Anusha. I am a learner. My passion is";
-	};
+	};*/
 };
 
 app.controller("BioController",BioController);
