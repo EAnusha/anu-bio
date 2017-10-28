e0edc2ed3e1aa160ff2bdbaabe84ef7bc0eb9b60
Anusha <anusha.e@yahoo.com>
2017-10-28 12:05:53 -0400

    added desc

--- pages/main.js
+++ pages/main.js
@@ -1,11 +1,10 @@
 var MainController = function($scope) {
 	$scope.currentState = "This is MainController";
+	$scope.desc = " This is about me";
 };
 
 
-var Description = function($scope){
- $scope.desc = "This is about me ....<span><a href="">more</a></span>"
-};
+
 
 app.controller("MainController",MainController);
 //module.exports = MainController;
