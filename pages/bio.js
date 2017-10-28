b00c94a77b5ead30af3bd39189b74c8e2c705096
Anusha <anusha.e@yahoo.com>
2017-10-28 12:54:27 -0400

    added desc

--- pages/bio.js
+++ pages/bio.js
@@ -1,7 +1,6 @@
 var BioController = function($scope) {
 	
-	$scope.desc = "Hello! Thanks for visiting my webpage.I am Anusha. I am a learner. My passion is ... 
-	to excel in Front End web development. Currently focusing on learning Angular framework.."
+	$scope.desc = "Hello! Thanks for visiting my webpage.I am Anusha. I am a learner. My passion is ... to excel in Front End web development. Currently focusing on learning Angular framework.."
 };
 
 app.controller("BioController",BioController);
