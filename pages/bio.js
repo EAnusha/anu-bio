6a170d3c256bf1d1c21ff2267e9fe21bbb03aae3
Anusha <anusha.e@yahoo.com>
2017-10-28 17:30:51 -0400

    another approach with hide

--- pages/bio.js
+++ pages/bio.js
@@ -14,11 +14,14 @@ var BioController = function($scope) {
 	};
 
 	$scope.isHide = function(clickAction){
-		if(currentBioState=="less" && clickAction=="more") {
+		/*if(currentBioState=="less" && clickAction=="more") {
 			return false;
 		}
 		
 		if(currentBioState=="more" && clickAction=="less") {
+			return false;
+		}*/
+		if(currentBioState!=clickAction) {
 			return false;
 		}
 		return true;
