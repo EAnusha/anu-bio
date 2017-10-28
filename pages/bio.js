eb84bc46ca536161899f639d822bafd83f6c3f88
Anusha <anusha.e@yahoo.com>
2017-10-28 17:27:48 -0400

    another approach with hide

--- pages/bio.js
+++ pages/bio.js
@@ -1,23 +1,30 @@
 var BioController = function($scope) {
 	
-	var shortBio = "Hello! Thanks for visiting my webpage.I am Anusha. I am a learner. My passion is";
-	var fullBio = "Hello! Thanks for visiting my webpage.I am Anusha. I am a learner. My passion is to excel in Front End web development. Currently focusing on learning Angular framework";
-	$scope.displayBio = shortBio;
-	$scope.displayBioButton = "Show More";
+	$scope.desc = "Hello! Thanks for visiting my webpage.I am Anusha. I am a learner. My passion is ...";
+	currentBioState= "less";
 
-	$scope.bioToggle = function() {
-		if($scope.displayBio==shortBio) {
-			$scope.displayBio = fullBio;
-			$scope.displayBioButton = "Show Less";
+	$scope.bioClickMore = function(){
+      $scope.desc = "Hello! Thanks for visiting my webpage.I am Anusha. I am a learner. My passion is to excel in Front End web development. Currently focusing on learning Angular framework";
+      currentBioState= "more"
+	};
+
+	$scope.bioClickLess = function(){
+      $scope.desc = "Hello! Thanks for visiting my webpage.I am Anusha. I am a learner. My passion is ...";
+      currentBioState= "less"
+	};
+
+	$scope.isHide = function(clickAction){
+		if(currentBioState=="less" and clickAction=="more") {
+			return false;
 		}
-		else if ($scope.displayBio==fullBio) {
-			$scope.displayBio = shortBio;
-			$scope.displayBioButton = "Show More";
-		};
-
+		
+		if(currentBioState=="more" and clickAction=="less") {
+			return false;
+		}
+		return true;
 	}
-
 };
+
 
 app.controller("BioController",BioController);
 
@@ -26,3 +33,4 @@ app.controller("BioController",BioController);
 
 
 //module.exports = MainController;
+//to excel in Front End web development. Currently focusing on learning Angular framework.."
