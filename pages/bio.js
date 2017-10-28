4ca091d02b532a38608dd84774f32fc4db0b7cc3
Anusha <anusha.e@yahoo.com>
2017-10-28 12:24:58 -0400

    added desc

--- pages/bio.js
+++ pages/bio.js
@@ -1,6 +1,8 @@
 var BioController = function($scope) {
 	
-	$scope.desc = " This is about me";
+	$scope.desc = "Hello! Thanks for visiting my webpage.
+
+I am Anusha. I am a learner. My passion is ... more."
 };
 
 
