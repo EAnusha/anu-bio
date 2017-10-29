94f8d9c51d17d406637f7fd1809365d20a832e2b
Anusha <anusha.e@yahoo.com>
2017-10-29 03:04:19 -0400

    background

--- pages/bio.js
+++ pages/bio.js
@@ -3,8 +3,9 @@ var BioController = function($scope) {
 	$scope.intro = "I'm Anusha.";
 	$scope.introTag = "An aspriring web developer.";
     $scope.shortBio = "Hello! Thanks for visiting my webpage.I am Anusha. I am a learner. My passion is to excel in Front End web development. Currently focusing on learning Angular framework";
-
+    $scope.showProfileContent = false;
 	$scope.myProfile = function(){
+		    $scope.showProfileContent = true;
 	};
 
 };
