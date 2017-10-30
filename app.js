f0b526acede61f9867d7c9c73a08ee611a0c4f14
Anusha <anusha.e@yahoo.com>
2017-10-30 19:44:46 -0400

    added home

--- app.js
+++ app.js
@@ -4,11 +4,12 @@ var app = angular.module('app',['ngRoute']);
 
 	var routeConfig = function($routeProvider){
     	$routeProvider
+        .when("/home",{templateUrl:"/pages/home.html"})
     	.when("/bio",{templateUrl:"/pages/bio.html"})
     	.when("/apps",{templateUrl:"/pages/apps.html"})
     	.when("/helpfullinks",{templateUrl:"/pages/helpfullinks.html"})
     	.when("/blog",{templateUrl:"/pages/blog.html"})
-    	.otherwise({redirectTo:"index.html"});
+    	.otherwise({redirectTo:"/home"});
   	}
 	
 	app.config(routeConfig);
